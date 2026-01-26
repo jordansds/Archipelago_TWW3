9b0677f22311de2c20afda10fa2cc13b0ba3cd0b
jordansds <56271228+jordansds@users.noreply.github.com>
2026-01-26 21:10:00 +0000

    Delete worlds/tww3 directory

--- worlds/tww3/item_tables/progression_table.py
+++
@@ -1,8 +0,0 @@
-from .items import ItemType, ItemData
-from BaseClasses import ItemClassification as IC
-from typing import NamedTuple, Dict, Set, List
-
-progression_table: Dict[int, ItemData] = {
-   1000: ItemData(IC.progression, 0, "Orb of Domination", ItemType.goal, "None", None, None),
-   1100: ItemData(IC.progression, 0, "Sphere of Influence", ItemType.progression, "None", None, None)
-}
